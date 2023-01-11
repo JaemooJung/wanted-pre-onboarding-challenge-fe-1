857887a00f392d59c95877bacd189cbcff52100b
Jaemoo Jung <jaemoojung@Jaemooui-MacBookPro.local>
2023-01-11 17:15:09 +0900

    TodoForm is extracted from AddTodoForm and EditTodoForm

--- want_to_do/src/components/Home/AddTodoForm.tsx
+++ want_to_do/src/components/Home/AddTodoForm.tsx
@@ -1,9 +1,11 @@
 import {useCreateTodo} from "../../hooks/todos";
 import {useState} from "react";
+import TodoForm from "./TodoForm";
 
 interface AddTodoFormProps {
   toggleAddTodo: () => void;
 }
+
 const AddTodoForm = ({toggleAddTodo}: AddTodoFormProps) => {
   const createTodo = useCreateTodo();
   const [title, setTitle] = useState("");
@@ -20,26 +22,15 @@ const AddTodoForm = ({toggleAddTodo}: AddTodoFormProps) => {
   }
 
   return (
-    <div className={`flex flex-col items-center justify-center 
-                    bg-neutral-200 p-4 m-2 rounded-lg`}
-    >
-      <span>Title</span>
-      <input className={`bg-neutral-100 w-2/3 h-8 rounded mb-4`}
-             type="text"
-             value={title}
-             onChange={(e) => setTitle(e.target.value)}
-      />
-      <span>Content</span>
-      <input className={`bg-neutral-100 w-2/3 h-8 rounded mb-6`}
-             type="text"
-             value={content}
-             onChange={(e) => setContent(e.target.value)}
-      />
-      <div className={`flex flex-row gap-8`}>
-        <button onClick={toggleAddTodo}> 취소 </button>
-        <button onClick={handleCreateTodo}> 추가 </button>
-      </div>
-    </div>
+    <TodoForm title={title}
+              content={content}
+              setTitle={setTitle}
+              setContent={setContent}
+              handleCancel={toggleAddTodo}
+              handleSubmit={handleCreateTodo}
+              cancelLabel={"취소"}
+              submitLabel={"추가"}
+    />
   )
 }
 
